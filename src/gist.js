9811c509ba3f12c95ccc095214e9ee1e878fc911
WrPages <wenderramirez00@gmail.com>
2026-04-01 03:14:56 -0600

    Update gist.js

--- src/gist.js
+++ src/gist.js
@@ -1,1 +1,26 @@
+import axios from "axios";
 
+const BASE = "https://api.github.com/gists";
+
+export async function getGist(gistId) {
+  const res = await axios.get(`${BASE}/${gistId}`, {
+    headers: { Authorization: `token ${process.env.GIST_TOKEN}` }
+  });
+
+  const file = Object.values(res.data.files)[0];
+  return file.content;
+}
+
+export async function updateGist(gistId, content) {
+  const res = await axios.patch(`${BASE}/${gistId}`, {
+    files: {
+      "tracking.json": {
+        content: JSON.stringify(content, null, 2)
+      }
+    }
+  }, {
+    headers: { Authorization: `token ${process.env.GIST_TOKEN}` }
+  });
+
+  return res.data;
+}
