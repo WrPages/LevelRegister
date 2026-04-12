9e654fb93ac10f27247c1608cc377727d24aed64
WrPages <wenderramirez00@gmail.com>
2026-04-12 02:31:29 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -872,7 +872,7 @@ const logoHeight = logo.height * (logoWidth / logo.width);
 
 // Centro perfecto
 const x = (baseImage.width - logoWidth) / 2;
-const y = (baseImage.height - logoHeight) / 2;
+const y = (baseImage.height - logoHeight);
 
 ctx.globalAlpha = 1;
 ctx.drawImage(logo, x, y, logoWidth, logoHeight);
